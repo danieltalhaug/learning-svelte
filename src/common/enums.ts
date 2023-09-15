4a904230c49fe20c84718150307f4b43e36c97e5
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-15 17:18:43 +0200

    Create button component

---
+++ src/common/enums.ts
@@ -0,0 +1,4 @@
+export enum buttonTypes {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+}
---
+++ src/lib/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    // types
+    import { buttonTypes } from '../common/enums';
+
+    export let type: buttonTypes;
+    const dispatch = createEventDispatcher();
+</script>
+
+<button
+    class="{type} px-8 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-75 transition border-2 border-indigo-600"
+    on:click={() => dispatch('clicked')}
+>
+    <slot />
+</button>
+
+<style lang="postcss">
+    .primary {
+        @apply bg-indigo-600 text-indigo-50 font-semibold;
+    }
+
+    .primary, .secondary {
+        @apply hover:bg-indigo-500 hover:text-indigo-50;
+    }
+</style>
