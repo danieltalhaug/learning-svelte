5cb50068be954c0a61141913ece2ce2eead78e1e
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-17 12:25:52 +0200

    Add in, out and flip animations to polls

--- src/lib/PollsList.svelte
+++ src/lib/PollsList.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
+    // types
     import type { poll } from '../common/types';
+    // Libs
+    import { fade } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
+    //Components
     import Poll from './Poll.svelte';
     import Card from './Card.svelte';
 
     export let polls: poll[];
 
+    const duration = 150;
+
 </script>
 
 <article class="grid md:grid-cols-2 lg:grid-cols-3 gap-12">
     {#each polls as poll (poll.id)}
-    <Card>
-        <Poll {poll} on:vote />
-    </Card>
+    <div
+        in:fade={{duration}}
+        out:fade={{duration}}
+        animate:flip={{duration}}
+    >
+        <Card>
+            <Poll {poll} on:vote on:delete />
+        </Card>
+    </div>
     {/each}
 </article>
 
