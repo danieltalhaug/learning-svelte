8cd7582984287444872231cee3107b696b00ef74
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-22 11:13:25 +0200

    Convert to using store

--- src/App.svelte
+++ src/App.svelte
@@ -10,84 +10,9 @@
     import Button from './lib/Button.svelte';
 
     let showAside = false;
-    let polls: poll[] = [
-        {
-            id: "EdmNyOwVF8OHhZm2fafY_",
-            question: "Favourite food?",
-            options: [
-                {
-                    id: "cl-JMyPaMWAYY4n5wgd4k",
-                    value: "Tacos",
-                    count: 4
-                },
-                {
-                    id: "1yYee_WLRcRfVCCSl8za8",
-                    value: "Pizza",
-                    count: 2
-                },
-                {
-                    id: "yDjbl7iYk3fPPy9hy0orc",
-                    value: "BBQ",
-                    count: 3
-                },
-                {
-                    id: "2zo9yBplLMay1tX6Ipmzh",
-                    value: "Hamburger",
-                    count: 9
-                }
-            ]
-        },
-        {
-            id: "N3MvjvlU-1cRHE5_suEHI",
-            question: "Favourite car brand?",
-            options: [
-                {
-                    id: "pugHH39eofm5ckQ-UZN-F",
-                    value: "Ferrari",
-                    count: 8
-                },
-                {
-                    id: "mehMHHZK8W39znFe7F4oC",
-                    value: "Lamborghini",
-                    count: 24
-                },
-                {
-                    id: "Dj9iHZoOmEsoG3cvHPyQg",
-                    value: "Aston Martin",
-                    count: 6
-                }
-            ]
-        }
-    ];
-
-    function handleVote(event: CustomEvent): void {    
-        const { pollId, optionId } = event.detail;
-        const poll = polls.find(poll => poll.id === pollId);
-        
-        poll?.options.find(option => {
-            if (option.id === optionId) {
-                return option.count++;
-            } 
-        });
-
-        // Trigger rerender
-        polls = polls;
-    }
-
-    function handleDelete(event: CustomEvent) {
-        polls = polls.filter(poll => poll.id !== event.detail);
-    }
 
     function toggleAside() {
         showAside = !showAside;
-    }
-
-    function onCreatePoll(event: CustomEvent) {
-        if (!event?.detail) {
-            return;
-        }
-
-        polls = [...polls, event.detail];
     }
 </script>
 
@@ -107,7 +32,7 @@
             </Button>
         </span>
     </header>
-    <PollsList {polls} on:vote={handleVote} on:delete={handleDelete} />
+    <PollsList />
 </AppContainer>
 
 <AppAside
@@ -116,7 +41,6 @@
     subtitle={'Add a question and the amount of options you want, submit to create a new poll.'}
 >
     <PollForm
-        on:submit={onCreatePoll}
         on:cancel={toggleAside}
     />
 </AppAside>
--- src/lib/PollsList.svelte
+++ src/lib/PollsList.svelte
@@ -2,27 +2,63 @@
     // types
     import type { poll } from '../common/types';
     // Libs
+    //import { onDestroy } from 'svelte';
     import { fade } from 'svelte/transition';
     import { flip } from 'svelte/animate';
+    // Stores
+    import PollStore from '../stores/PollStore';
     //Components
     import Poll from './Poll.svelte';
     import Card from './Card.svelte';
 
-    export let polls: poll[];
-
     const duration = 150;
 
+    function handleVote(event: CustomEvent): void {
+        const { pollId, optionId } = event.detail;
+
+        PollStore.update(polls => {
+            const poll = polls.find(poll => poll.id === pollId);
+            poll?.options.find(option => {
+                if (option.id === optionId) {
+                    return option.count++;
+                } 
+            });
+
+            return [...polls];
+        });
+    }
+
+    function handleDelete(event: CustomEvent) {
+        PollStore.update(polls => {
+            return polls.filter(poll => poll.id !== event.detail);
+        });
+    }
+
+    /*
+    let polls: poll[] = [];
+
+    Subscribe to store, must also remember to unsubscribe on distroy to prevent memory leaks
+    and performance issues
+
+    const unsub = PollStore.subscribe(data => {
+        polls = data;
+    });
+
+    onDestroy(() => {
+        unsub();
+    });
+    */
 </script>
 
 <article class="grid md:grid-cols-2 lg:grid-cols-3 gap-12">
-    {#each polls as poll (poll.id)}
+    {#each $PollStore as poll (poll.id)}
     <div
         in:fade={{duration}}
         out:fade={{duration}}
         animate:flip={{duration}}
     >
         <Card>
-            <Poll {poll} on:vote on:delete />
+            <Poll {poll} on:vote={handleVote} on:delete={handleDelete} />
         </Card>
     </div>
     {/each}
